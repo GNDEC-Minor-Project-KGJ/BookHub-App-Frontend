93fc8031bca96225ce23db5f7131e90dc4bc7783
Konark Lohat <konarklohat123456@gmail.com>
2022-12-02 18:01:54 +0530

    Backend seed

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+//My routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const paymentBRoutes = require("./routes/paymentBRoutes");
+
+//DB Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+//Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(morgan("dev"));
+
+//My Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", paymentBRoutes);
+
+//PORT
+const port = process.env.PORT || 5000;
+
+//Starting a server
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
